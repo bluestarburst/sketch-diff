0b50217c5810a6fcee8cbb22b9ef8063004ab02e
bluestarburst <hargreaves.bryant@gmail.com>
2023-03-29 21:38:24 -0500

    Co-authored-by: Adi-K527 <Adi-K527@users.noreply.github.com> Co-authored-by: Nicole Iwuala <nikkinikki247@users.noreply.github.com> Co-authored-by: Srinesh Ganesh <srinashg@users.noreply.github.com>

--- src/Doodle.js
+++ src/Doodle.js
@@ -1,12 +1,33 @@
 import CanvasDraw from "react-canvas-draw";
-
-import React from 'react'
+import './Doodle.css';
+import {useRef} from 'react'
+import { useEffect, useState } from "react";
+import Title from "./Title"
 
 export default function Doodle() {
+
+  const [imgUrl, setImgURL] = useState("")
+
+  useEffect(() => {
+    document.title = "Team Name";  
+  }, [imgUrl]);
+
+
+
+  const ref = useRef(null)
+
+  const getImageData = () => {
+    console.log(ref.current.getSaveData())
+    console.log(ref.current.getDataURL())
+    setImgURL(ref.current.getDataURL())
+  }
+
   return (
-    <div>
-        <CanvasDraw style={{ boxShadow: "0 13px 27px -5px rgba(50, 50, 93, 0.25), 0 8px 16px -8px rgba(0, 0, 0, 0.3)" }} />
-        
+    <div className="canvas">
+
+        <CanvasDraw ref={ref} style={{ boxShadow: "0 13px 27px -5px rgba(50, 50, 93, 0.25), 0 8px 16px -8px rgba(0, 0, 0, 0.3)" }} />
+        <button className="" onClick={getImageData}>Click</button>
+        <img src={imgUrl}/>
     </div>
   )
 }
---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Title() {
+    return (
+        <div className="Title">
+          <h1>What'sSupPeeps</h1>
+        </div>
+      );
+}
+
+
+
