906bbc2b2006d4905e3e1bc8432d59e006ca9578
bluestarburst <hargreaves.bryant@gmail.com>
2023-04-02 16:53:58 -0500

    Co-authored-by: Adi-K527 <Adi-K527@users.noreply.github.com> Co-authored-by: Srinesh Ganesh <srinashg@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Doodle from './Doodle';
 import Title from './Title';
+import UI from './UI';
+import { useState } from 'react';
 
 function App() {
 
   // model code should go here probably!
-  
+  const [brushSize, setVar] = useState(1);
   // and other stuff too!
 
   return (
@@ -15,7 +17,8 @@ function App() {
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <Title/>
         <Doodle/>
-        { /* a button to save the doodle */}
+        <UI brushSize={brushSize} setVar={setVar}/>
+        { /*  */}
         
       </header>
     </div>
--- src/Doodle.js
+++ src/Doodle.js
@@ -1,33 +1,107 @@
 import CanvasDraw from "react-canvas-draw";
 import './Doodle.css';
-import {useRef} from 'react'
+import { useRef } from 'react'
 import { useEffect, useState } from "react";
 import Title from "./Title"
 
+import * as tf from '@tensorflow/tfjs';
+
 export default function Doodle() {
 
+  
+
   const [imgUrl, setImgURL] = useState("")
+  const [greyScaleURL, setGreyScaleURL] = useState("")
 
   useEffect(() => {
-    document.title = "Team Name";  
-  }, [imgUrl]);
+    document.title = "Team Name";
+  }, [imgUrl, greyScaleURL]);
+
 
 
 
   const ref = useRef(null)
+  const canvasRef = useRef(null)
 
-  const getImageData = () => {
-    console.log(ref.current.getSaveData())
-    console.log(ref.current.getDataURL())
-    setImgURL(ref.current.getDataURL())
+  const getImageData = async () => {
+
+    const imgurl = await ref.current.getDataURL()
+    setImgURL(imgurl)
+
+    const img = document.createElement('img');
+    img.src = imgurl
+
+    var imgData
+
+    img.addEventListener('load', () => {
+      const canvas = document.createElement("canvas")
+      const ctx = canvas.getContext("2d")
+
+      canvas.width = 28
+      canvas.height = 28
+
+      ctx.drawImage(img, 0, 0, 28, 28)
+
+      imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+      console.log(imgData)
+
+      for(var y = 0; y < canvas.height; y++){
+        for(var x = 0; x < canvas.width; x++){
+            var i = (y * 4) * canvas.width + x * 4;
+            var avg = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3;
+            imgData.data[i] = avg;
+            imgData.data[i + 1] = avg;
+            imgData.data[i + 2] = avg;
+        }
+    }
+
+      ctx.putImageData(imgData, 0, 0, 0, 0, canvas.width, canvas.height)
+      console.log(ctx.getImageData(0, 0, canvas.width, canvas.height))
+
+      const url = canvas.toDataURL(0, 0, canvas.width, canvas.height)
+
+      setGreyScaleURL(url) // here
+
+      canvas.remove()
+      img.remove()
+
+    })
+
+    console.log(greyScaleURL)
+    console.log("hit end")
+
+    const model = await tf.loadLayersModel('model/model.json')
+
+    const pred = model.predict(preprocess(imgData)).dataSync()
+    console.log(pred)
+
   }
+
+  const preprocess = (imgData) => {
+    return tf.tidy(()=>{
+      //convert the image data to a tensor 
+      let tensor = tf.browser.fromPixels(imgData, 1)
+      //resize to 28 x 28 
+      const resized = tf.image.resizeBilinear(tensor, [28, 28]).toFloat()
+      // Normalize the image 
+      const offset = tf.scalar(255.0);
+      const normalized = tf.scalar(1.0).sub(resized.div(offset));
+      //We add a dimension to get a batch shape 
+      const batched = normalized.expandDims(0)
+      return batched
+    })
+  }
+
+
 
   return (
     <div className="canvas">
+      <CanvasDraw brushColor="#600" ref={ref} hideGrid style={{ boxShadow: "0 13px 27px -5px rgba(50, 50, 93, 0.25), 0 8px 16px -8px rgba(0, 0, 0, 0.3)"}} />
+      <button className="" onClick={getImageData}>Click</button>
+      <img src={imgUrl} />
+      <img src={greyScaleURL} />
 
-        <CanvasDraw ref={ref} style={{ boxShadow: "0 13px 27px -5px rgba(50, 50, 93, 0.25), 0 8px 16px -8px rgba(0, 0, 0, 0.3)" }} />
-        <button className="" onClick={getImageData}>Click</button>
-        <img src={imgUrl}/>
     </div>
   )
 }
